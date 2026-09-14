a1e21ed87843f96222a20ba8476158d8d3afa9f1
paulohhs <paulohhonoratos@gmail.com>
2026-09-14 19:33:38 -0300

    testes(tests/customer/usecase/update.customer.unit.spec.ts): adiciona testes unitários para update customer usecase.

---
+++ tests/customer/usecase/update.customer.unit.spec.ts
@@ -0,0 +1,39 @@
+import CustomerFactory from "../../../src/domain/customer/factory/customer.factory";
+import Address from "../../../src/domain/customer/value-object/address";
+import UpdateCustomerUseCase from "../../../src/usecase/customer/update/update.customer.usecase";
+
+const customer = CustomerFactory.createWithAddress(
+    "John",
+    new Address("Street", 1, "Zip", "City")
+);
+
+const input = {
+    id: customer.id,
+    name: "John Updated",
+    address: {
+        street: "Street Updated",
+        number: 2,
+        zip: "Zip Updated",
+        city: "City Updated",
+    }
+};
+
+const MockRepository = () => {
+    return {
+        find: jest.fn().mockReturnValue(Promise.resolve(customer)),
+        findAll: jest.fn(),
+        create: jest.fn(),
+        update: jest.fn(),
+        delete: jest.fn(),
+    }
+}
+
+describe("Unit test update customer use case", () => {
+    it("should update a customer", async () => {
+        const customerRepository = MockRepository();
+        const customerUpdateUseCase = new UpdateCustomerUseCase(customerRepository);
+
+        const output = await customerUpdateUseCase.execute(input);
+        expect(output).toEqual(input);
+    });
+});
